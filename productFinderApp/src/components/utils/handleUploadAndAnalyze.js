b2e0314e753638c281d4cd80b310f6f8ad268904
Michael Dawson <145623651+mikewd-dev@users.noreply.github.com>
2025-09-19 14:25:55 +0000

    attempt at fix

--- productFinderApp/src/components/utils/handleUploadAndAnalyze.js
+++ productFinderApp/src/components/utils/handleUploadAndAnalyze.js
@@ -1,4 +1,4 @@
-import { handleImageUpload } from "./imageHandlingAndApiCall";
+/* import { handleImageUpload } from "./imageHandlingAndApiCall";
 
 // This function will run when you click "Upload and Analyze"
 export const handleUploadAndAnalyze = async (
@@ -66,4 +66,40 @@ export const handleUploadAndAnalyze = async (
   } finally {
     setLoading(false);
   }
-};
+}; */
+
+import { handleImageUpload } from "./imageHandlingAndApiCall";
+
+export const handleUploadAndAnalyze = async (selectedImage, setProductName, setError, setLoading,setProductData, setAnalysisResults) => {
+    if (!selectedImage) {
+      alert('Please select an image first.');
+      return;
+    }
+
+    const utilResponse = await handleImageUpload(selectedImage, setProductName, setError, setLoading);
+    setProductData(utilResponse);
+
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      const base64Image = reader.result.split(',')[1];
+      const apiKey = import.meta.env.VITE_REACT_APP_GOOGLE_VISION_API;
+
+      const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          requests: [{ image: { content: base64Image }, features: [{ type: 'WEB_DETECTION' }] }],
+        }),
+      });
+
+      const responseData = await response.json();
+
+
+      setAnalysisResults(responseData.responses[0].webDetection);
+    };
+
+    reader.readAsDataURL(selectedImage);
+  };
