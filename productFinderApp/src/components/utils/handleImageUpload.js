b31ea1eba08429980df0dcf12097b4d2adfeba13
Michael Dawson <145623651+mikewd-dev@users.noreply.github.com>
2025-09-21 22:30:02 +0000

    adding a console.log to check file

--- productFinderApp/src/components/utils/handleImageUpload.js
+++ productFinderApp/src/components/utils/handleImageUpload.js
@@ -76,7 +76,7 @@ export const handleImageUpload = async (imageFile, setProductName, setError, set
 
           const data = await response.json();
           console.log("Raw API response:", data);
-          return data;
+          resolve (data);
         } catch (err) {
           reject(err);
         }
