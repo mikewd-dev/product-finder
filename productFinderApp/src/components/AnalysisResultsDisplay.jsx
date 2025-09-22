04cb14e1ddc6d659e00e340378b8bc3427fd9c4f
Michael Dawson <145623651+mikewd-dev@users.noreply.github.com>
2025-09-22 12:38:21 +0000

    displays all results inside div

--- productFinderApp/src/components/AnalysisResultsDisplay.jsx
+++ productFinderApp/src/components/AnalysisResultsDisplay.jsx
@@ -10,46 +10,27 @@ function AnalysisResults({ analysisResults }) {
     })) || [];
 
   return (
-    <div>
-      {pagesWithMatchingImages.length > 0 && (
-        <div className="matching-images">
-          <h2>Web Pages with Matching Images:</h2>
-          <ul className="matching-list">
-            {pagesWithMatchingImages.map((page, index) => (
-              <li key={index}>
-                <a href={page.url} target="_blank" rel="noopener noreferrer">
-                  {page.url}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {visuallySimilarImages.length > 0 && (
-        <div className="visually-similar">
-          <h2>Visually Similar Images:</h2>
-          <ul>
-            {visuallySimilarImages.map((image, index) => (
-              <li key={index}>
-                {image.url && <img src={image.url} alt={`Matching Image ${index + 1}`} />}
-                {image.url && (
-                  <p>
-                    <a href={image.url} target="_blank" rel="noopener noreferrer">
-                      View Image
-                    </a>
-                  </p>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {pagesWithMatchingImages.length === 0 && visuallySimilarImages.length === 0 && (
-        <p>No analysis results to display.</p>
-      )}
-    </div>
+    <div className="visually-similar">
+  <h2>Visually Similar Images:</h2>
+  {visuallySimilarImages && visuallySimilarImages.length > 0 ? (
+    <ul>
+      {visuallySimilarImages.map((image, index) => (
+        <li key={index}>
+          {image.url && <img src={image.url} alt={`Matching Image ${index + 1}`} />}
+          {image.url && (
+            <p>
+              <a href={image.url} target="_blank" rel="noopener noreferrer">
+                View Image
+              </a>
+            </p>
+          )}
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p>No visually similar images to display.</p>
+  )}
+</div>
   );
 }
 
