cb84502d6d986418cc00c3e51b3168964c425d23
Michael Dawson <145623651+mikewd-dev@users.noreply.github.com>
2025-09-25 11:55:22 +0000

    made a fix

--- productFinderApp/src/components/utils/handleUploadAndAnalyze.js
+++ productFinderApp/src/components/utils/handleUploadAndAnalyze.js
@@ -1,5 +1,5 @@
 import { fetchProductsFromImage } from "./fetchProducts";
 
-export const handleImage = (imageFile, setProductName, setError, setLoading) => {
+export const handleUpload = (imageFile, setProductName, setError, setLoading) => {
   return fetchProductsFromImage(imageFile, setProductName, setError, setLoading);
 };
